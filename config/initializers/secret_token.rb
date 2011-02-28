eded5f5baf6ca64d5c68e9f36305a7bf627e0e60
JunyongSuh <junyong.suh@headflow.net>
2011-02-28 11:43:05 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Creamkit::Application.config.secret_token = '4f72e3f95a94a396587bda0b731e311c1aa735a03bf19836bc2750e697d37f866899b6ff4b752772bde0b9b08b5069273ac380a223f802000c5fcbd2905daf03'
